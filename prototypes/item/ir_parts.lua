15e54233c31ff0ef7681409858e88d8be545ee26
jatmn <the@jat.mn>
2025-04-05 15:13:28 -0700

    fixed recipe grouping and names for some turrets. fixed up main_product in r_defense. code cleanup r_defense. fixed some recipe grouping and names for intermediates. code cleanup ir_parts.

--- prototypes/item/ir_parts.lua
+++ prototypes/item/ir_parts.lua
@@ -1,81 +1,107 @@
 data:extend(
 {
-	
+
 	{
 		type = "recipe",
 		name = "y-bluegear",
-		enabled = true,	energy_required = 0.5,  group = "yuoki", subgroup = "y-parts", order = "p-g1",  icon_size = 64, icon =  "__Yuoki__/graphics/icons/bluegear-icon.png",    		
+		enabled = true,
+		energy_required = 0.5,
+		group = "yuoki",
+		subgroup = "y-parts",
+		order = "p-g1",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/bluegear-icon.png",
 		ingredients ={
 			{type="item", name="steel-plate", amount=1},
 			{type="item", name="y-refined-yres1", amount=1},
 		},
-		results = {{type="item", name="y-bluegear", amount=3},},
+		results = {
+			{type="item", name="y-bluegear", amount=3},
+		},
 		main_product = "y-bluegear",
-		--result = "y-bluegear",
-		--result_count = 3, 
-	},		
+	},
+
 	{
 		type = "recipe",
 		name = "y_structure_element",
-		enabled = true,	energy_required = 0.5,  group = "yuoki", subgroup = "y-parts", order = "p-g2",  icon_size = 64, icon =  "__Yuoki__/graphics/icons/structur_element.png",    		
+		enabled = true,
+		energy_required = 0.5,
+		group = "yuoki",
+		subgroup = "y-parts",
+		order = "p-g2",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/structur_element.png",
 		ingredients ={
 			{type="item", name="iron-plate", amount=4},
 			{type="item", name="y-unicomp-raw", amount=2},
 		},
-		results = {{type="item", name="y_structure_element", amount=3},},
+		results = {
+			{type="item", name="y_structure_element", amount=3},
+		},
 		main_product = "y_structure_element",
-		--result = "y_structure_element",
-		--result_count = 3, 
-	},		
+	},
+
 	{
 		type = "recipe",
 		name = "y_structure_vessel",
-		enabled = true,	energy_required = 0.5,  group = "yuoki", subgroup = "y-parts", order = "p-g3",  icon_size = 64, icon =  "__Yuoki__/graphics/icons/vessel_structure_icon.png",    		
+		enabled = true,
+		energy_required = 0.5,
+		group = "yuoki",
+		subgroup = "y-parts",
+		order = "p-g3",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/vessel_structure_icon.png",
 		ingredients ={
 			{type="item", name="steel-plate", amount=2},
 			{type="item", name="y-unicomp-raw", amount=1},
 		},
-		results = {{type="item", name="y_structure_vessel", amount=2},},
+		results = {
+			{type="item", name="y_structure_vessel", amount=2},
+		},
 		main_product = "y_structure_vessel",
-		--result = "y_structure_vessel", result_count = 2, 
-	},		
-	
+	},
+
 	-- basic stage-1 maschine frame
 	{
 		type = "recipe",
-		name = "y-basic-st1-mf",
+		name = "y-basic-t1-mf",
 		ingredients = {
 			{type="item", name="y-conductive-wire-1", amount=3},
 			{type="item", name="y-bluegear", amount=3},
 			{type="item", name="y_structure_element", amount=2}
 		},
-		results = {{type="item", name="y-basic-t1-mf", amount=1},},
-		--result = "y-basic-t1-mf",
-		enabled = true,		
-		--result_count = 1,
+		results = {
+			{type="item", name="y-basic-t1-mf", amount=1},
+		},
+		main_product = "y-basic-t1-mf",
+		enabled = true,
 		group = "yuoki",
-		subgroup = "y-parts",					
+		subgroup = "y-parts",
 		order="p-g4",
-	},	
-	
+	},
+
 	-- basic stage-2 maschine frame
 	{
 		type = "recipe",
-		name = "y-basic-st2-mf",
-		icon_size = 64, icon =  "__Yuoki__/graphics/icons/maschine-frame-t2.png",
+		name = "y-basic-t2-mf",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/maschine-frame-t2.png",
 		ingredients = {
 			{type="item", name="y-conductive-coil-1", amount=2},
 			{type="item", name="y_structure_electric", amount=3},
 			{type="item", name="y-bluegear", amount=4}
 		},
-		results = { {type="item", name="y-basic-t2-mf", amount=1,}, {type="item", name="y_rwtechsign", amount=1,}, },			
-		main_product = "y-basic-t2-mf",						
-		enabled = true,		
+		results = {
+			{type="item", name="y-basic-t2-mf", amount=1,},
+			{type="item", name="y_rwtechsign", amount=1,},
+		},
+		main_product = "y-basic-t2-mf",
+		enabled = true,
 		group = "yuoki",
-		subgroup = "y-parts",					
+		subgroup = "y-parts",
 		order="p-g5",
-	},	
-	
+	},
+
 	-- Heat-Cool-Rib-Block
 	{
 		type = "recipe",
@@ -84,20 +110,22 @@ data:extend(
 			{type="item", name="copper-plate", amount=5},
 			{type="item", name="iron-plate", amount=2},
 		},
-		results = {{type="item", name="y-heat-pipe", amount=2},},
-		--result = "y-heat-pipe",
-		enabled = true,		
-		--result_count = 2,
+		results = {
+			{type="item", name="y-heat-pipe", amount=2},
+		},
+		main_product = "y-heat-pipe",
+		enabled = true,
 		order="p-g6",
-		subgroup = "y_parts_e",	
-	},	
+		subgroup = "y_parts_e",
+	},
 	{
 		type = "item",
 		name = "y-heat-pipe",
-		icon_size = 64, icon =  "__Yuoki__/graphics/icons/heat-pipe.png",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/heat-pipe.png",
 		order="p-g6",
-		subgroup = "y_parts_e",	
-		stack_size = 100, default_request_amount = 20, 
-	},	
-	
+		subgroup = "y_parts_e",
+		stack_size = 100, default_request_amount = 20,
+	},
+
 })
--- prototypes/recipe/r_defense.lua
+++ prototypes/recipe/r_defense.lua
@@ -10,15 +10,17 @@ data:extend(
 		ingredients = {
 			{type="item", name="y_slag_brick", amount=8},
 			{type="item", name="steel-plate", amount=1}
-		}, 
-		results = {{type="item", name="y-mud-wall", amount=1},},
-		--result = "y-mud-wall",
-		--result_count = 1,
-		 icon_size = 64, icon =  "__Yuoki__/graphics/icons/m_wall_icon.png",
+		},
+		results = {
+			{type="item", name="y-mud-wall", amount=1},
+		},
+		main_product = "y-mud-wall",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/m_wall_icon.png",
 		subgroup = "y_defense_walls",
 		order="w-a",
-		--category = "yuoki-formpress-recipe",		
-	},	
+	},
+
 	{
 		type = "recipe",
 		name = "y-rare-wall-basic",
@@ -28,134 +30,159 @@ data:extend(
 			{type="item", name="y-refined-yres1", amount=1},
 			{type="item", name="y-slag", amount=6},
 			{type="item", name="y-mud-wall", amount=1}
-		}, 
-		results = {{type="item", name="y-rare-wall-basic", amount=1},},
-		--result = "y-rare-wall-basic",
-		subgroup = "y_defense_walls",	
-		--result_count = 1,
+		},
+		results = {
+			{type="item", name="y-rare-wall-basic", amount=1},
+		},
+		main_product = "y-rare-wall-basic",
+		subgroup = "y_defense_walls",
 		order="w-b",
-	},	
+	},
+
 	{
 		type = "recipe",
 		name = "y-rare-wall-adv",
-		 icon_size = 64, icon =  "__Yuoki__/graphics/icons/rare2-wall-icon2.png",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/rare2-wall-icon2.png",
 		energy_required = 2.0,
 		enabled = true,
 		ingredients = {
 			{type="item", name="y-orange-stuff", amount=6},
 			{type="item", name="y-slag", amount=6},
 			{type="item", name="y-mud-wall", amount=1}
-		}, 
-		results = { {type="item", name="y-rare-wall-adv", amount=1,}, {type="item", name="y_rwtechsign", amount=1,}, },	
-		main_product = "y-rare-wall-adv",				
-		subgroup = "y_defense_walls",			
+		},
+		results = {
+			{type="item", name="y-rare-wall-adv", amount=1,},
+			{type="item", name="y_rwtechsign", amount=1,},
+		},
+		main_product = "y-rare-wall-adv",
+		subgroup = "y_defense_walls",
 		order="w-c",
-	},		
+	},
+
 	{
 		type = "recipe",
 		name = "y-wall-forcefield",
-		 icon_size = 64, icon =  "__Yuoki__/graphics/icons/wall-forcefield-icon.png",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/icons/wall-forcefield-icon.png",
 		energy_required = 3.0,
 		enabled = true,
 		ingredients = {
 			{type="item", name="y-rare-wall-basic", amount=1},
 			{type="item", name="y-conductive-coil-1", amount=3},
 			{type="item", name="y-conductive-wire-1", amount=1},
-		}, 
-		results = { {type="item", name="y-wall-forcefield", amount=1,}, {type="item", name="y_rwtechsign", amount=1,}, },			
-		main_product = "y-wall-forcefield",		
-		subgroup = "y_defense_walls",			
-		order="w-d",		
-	},		
+		},
+		results = {
+			{type="item", name="y-wall-forcefield", amount=1,},
+			{type="item", name="y_rwtechsign", amount=1,},
+		},
+		main_product = "y-wall-forcefield",
+		subgroup = "y_defense_walls",
+		order="w-d",
+	},
+
 	{
 		type = "recipe",
 		name = "y_wall22_hardic",
-		 icon_size = 64, icon =  "__Yuoki__/graphics/entity/walls/wall_hardic_icon.png",
+		icon_size = 64,
+		icon = "__Yuoki__/graphics/entity/walls/wall_hardic_icon.png",
 		energy_required = 2.0,
 		enabled = true,
 		ingredients = {
 			{type="item", name="y_structure_vessel", amount=4},
 			{type="item", name="stone-brick", amount=4},
 			{type="item", name="y-richdust", amount=4}
-		}, 
-		results = { {type="item", name="y_wall22_hardic", amount=1,}, {type="item", name="y_rwtechsign", amount=1,}, },		
-		main_product = "y_wall22_hardic",				
-		subgroup = "y_defense_walls",			
+		},
+		results = {
+			{type="item", name="y_wall22_hardic", amount=1,},
+			{type="item", name="y_rwtechsign", amount=1,},
+		},
+		main_product = "y_wall22_hardic",
+		subgroup = "y_defense_walls",
 		order="w-f",
-	},	
-	
+	},
+
 	{
 		type = "recipe",
-		name = "y_laser_onhwall",
+		name = "y-laser-def-s4",
 		enabled = true,
 		energy_required = 4,
 		ingredients =
 		{
-			{type="item", name="y-basic-t2-mf", amount=1},	
+			{type="item", name="y-basic-t2-mf", amount=1},
 			{type="item", name="y_blocked_capa", amount=2},
-			{type="item", name="y-conductive-coil-1", amount=3},			
+			{type="item", name="y-conductive-coil-1", amount=3},
 			{type="item", name="y_wall22_hardic", amount=1},
 			{type="item", name="y_rwtechsign", amount=2},
 		},
-		results = {{type="item", name="y-laser-def-s4", amount=1},},
-		--result = "y-laser-def-s4",
+		results = {
+			{type="item", name="y-laser-def-s4", amount=1},
+		},
+		main_product ="y-laser-def-s4",
 		order="t-c",
-	},	
-	
+	},
+
 	{
 		type = "recipe",
-		name = "y_laser_mk1",
+		name = "y_turret_laser22f12",
 		enabled = true,
 		energy_required = 4,
 		ingredients =
 		{
-			{type="item", name="y-basic-t2-mf", amount=1},	
+			{type="item", name="y-basic-t2-mf", amount=1},
 			{type="item", name="y_blocked_capa", amount=2},
-			{type="item", name="y-conductive-coil-1", amount=3},			
+			{type="item", name="y-conductive-coil-1", amount=3},
 			{type="item", name="y_structure_electric", amount=4},
 			{type="item", name="y_rwtechsign", amount=1},
 		},
-		results = {{type="item", name="y_turret_laser22f12", amount=1},},
-		--result = "y_turret_laser22f12",
+		results = {
+			{type="item", name="y_turret_laser22f12", amount=1},
+		},
+		main_product = "y_turret_laser22f12",
 		order="t-c",
-	},	
-		
+	},
+
 	{
 		type = "recipe",
-		name = "y-gun-turret-mk2",
+		name = "y_turret_gun2f12",
 		enabled = true,
 		energy_required = 4,
 		ingredients =
-		{			
-			{type="item", name="y-basic-t1-mf", amount=1},						
+		{
+			{type="item", name="y-basic-t1-mf", amount=1},
 			{type="item", name="y_structure_element", amount=12},
-			{type="item", name="y-bluegear", amount=3},			
+			{type="item", name="y-bluegear", amount=3},
 			{type="item", name="y_rwtechsign", amount=1},
 		},
-		results = {{type="item", name="y_turret_gun2f12", amount=1},},
-		--result = "y_turret_gun2f12",
+		results = {
+			{type="item", name="y_turret_gun2f12", amount=1},
+		},
+		main_product = "y_turret_gun2f12",
 		order="t-a",
-	},	
-	
+	},
+
 	{
 		type = "recipe",
-		name = "y-gun-turret-mk3",
+		name = "y_turret_gun1f12",
 		enabled = true,
 		energy_required = 4,
 		ingredients =
-		{			
-			{type="item", name="y-basic-t1-mf", amount=1},					
-			{type="item", name="y_structure_element", amount=12},			
+		{
+			{type="item", name="y-basic-t1-mf", amount=1},
+			{type="item", name="y_structure_element", amount=12},
 			{type="item", name="steel-plate", amount=4},
 			{type="item", name="y_rwtechsign", amount=1},
 		},
-		results = {{type="item", name="y_turret_gun1f12", amount=1},},
-		--result = "y_turret_gun1f12",
+		results = {
+			{type="item", name="y_turret_gun1f12", amount=1},
+		},
+		main_product = "y_turret_gun1f12",
 		order="t-b",
-	},	
+	},
+
 	{
 		type = "recipe",
-		name = "y-weapon-ztt",		
+		name = "y-weapon-ztt",
 		energy_required = 3.0,
 		enabled = true,
 		ingredients = {
@@ -165,14 +192,13 @@ data:extend(
 			{type="item", name="y-accumulator-b-t2", amount=1},
 			{type="item", name="y-infused-uca2", amount=1},
 			{type="item", name="y_rwtechsign", amount=5},
-		}, 
-		results = {{type="item", name="y-weapon-ztt", amount=1},},
-		--result = "y-weapon-ztt",
-		subgroup = "y-defense",	
-		--result_count = 1,
+		},
+		results = {
+			{type="item", name="y-weapon-ztt", amount=1},
+		},
+		main_product = "y-weapon-ztt",
+		subgroup = "y-defense",
 		order="t-d",
-	},		
-	
-	
-}
-)	
+	},
+
+})
