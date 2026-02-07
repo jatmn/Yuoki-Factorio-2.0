69225c85532a03d259660d53ae120df51ceeb151
JATMN <the@jat.mn>
2026-02-07 09:05:59 -0800

    Integrations:     - Bob's Ores / Bob's Ores + Angel's - fixed missing Atomic's recipe for Thorium ore     - Bob's Ores + Angel's Refining - fixed missing Atomic's recipes for Bob's Ores     - Angel's Refining - Yi slag is now same item as Angel's slag     - Angel's Petrochemical Processing - Updated Atomics icons to match related Angel's liquids/gas's, still uses old naming sorry for the confusion. Maybe next update?   Changes:     - Removed optional dependencies for overhaul mods and added them to the mod description to address Circular Dependency loops by those same mods.     - LOTS of code refactoring and expansion of internal functions library...

--- prototypes/integration/bobores/ir_atomics.lua
+++ prototypes/integration/bobores/ir_atomics.lua
@@ -44,13 +44,28 @@ if mods["bobores"] then
 		yi.lib.recipe.atomics.recipes_make_item("bob-zinc-ore", 32, 20, 1) -- Zinc ore
 	end
 
-	-- This does not work right now, not sure why.. 
-	--if settings.startup["bobmods-ores-enablethoriumore"].value == true then
-	--	yi.lib.recipe.atomics.recipes_make_item("bob-thorium-ore", 32, 10, 1) -- Thorium ore
-	--end
+	if settings.startup["bobmods-ores-enablethoriumore"].value == true then
+		yi.lib.recipe.atomics.recipes_make_item("bob-thorium-ore", 32, 10, 1) -- Thorium ore
+	end
 
 	-- Fluids
 	if settings.startup["bobmods-ores-enablewaterores"].value == true then
 		yi.lib.recipe.atomics.recipes_make_fluid("bob-lithia-water", 32, 80, 1) -- Lithia water
+	end
+
+	if mods["angelsrefining"] then
+		-- Ores
+		yi.lib.recipe.atomics.recipes_make_item("bob-bauxite-ore", 32, 20, 1) -- Bauxite
+		yi.lib.recipe.atomics.recipes_make_item("bob-cobalt-ore", 32, 10, 1) -- Cobalt ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-gold-ore", 32, 10, 1) -- Gold ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-lead-ore", 32, 20, 1) -- Lead ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-nickel-ore", 32, 20, 1) -- Nickel ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-quartz", 32, 20, 1) -- Quartz
+		yi.lib.recipe.atomics.recipes_make_item("bob-rutile-ore", 32, 20, 1) -- Rutile ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-silver-ore", 32, 20, 1) -- Silver ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-tin-ore", 32, 20, 1) -- Tin ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-tungsten-ore", 32, 10, 1) -- Tungsten ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-zinc-ore", 32, 20, 1) -- Zinc ore
+		yi.lib.recipe.atomics.recipes_make_item("bob-thorium-ore", 32, 10, 1) -- Thorium ore
 	end
 end
